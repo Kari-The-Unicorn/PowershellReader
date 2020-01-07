652100879968119ea029a4c6268b69b89a4f7684
gh-unicorn <inmatrixos@gmail.com>
2020-01-07 21:46:40 +0000

    ps file with user's input reader to file

---
+++ TextReader.ps1
@@ -0,0 +1,12 @@
+﻿Add-Type -AssemblyName Microsoft.VisualBasic
+Add-Type -AssemblyName System.speech
+$User = [Microsoft.VisualBasic.Interaction]::InputBox('Enter a text here', 'User', "$env:UserName")
+
+if ($User -ne $null){
+#echo "Input was [$User]"
+$speak = New-Object System.Speech.Synthesis.SpeechSynthesizer
+    $speak.SetOutputToWaveFile("C:\Users\Administrator\Desktop\test.wav")
+    $speak.selectvoice("Microsoft Hazel Desktop")
+    $speak.Speak($User)
+    $speak.Dispose()
+}
